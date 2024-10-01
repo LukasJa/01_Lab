d82fc696bfed28442a0926d6c5dbb7e4e26a9556
Lukas <lukajaku0@gmail.com>
2024-10-01 23:10:07 +0300

    02

--- app/src/main/java/com/example/new_project_01/MainActivity.kt
+++ app/src/main/java/com/example/new_project_01/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.new_project_01
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnChangeText: Button
+    private lateinit var textMain: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnChangeText = findViewById(R.id.btnChangeText)
+        textMain = findViewById(R.id.textMain)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btnChangeText.setOnClickListener {
+            textMain.text = "Hello"
+        }
+
     }
 }
